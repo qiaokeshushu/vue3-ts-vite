2cf3fffbfadbcd077447d86de2c413f1299cfb54
乔克叔叔nic <qiaokeshushunic@qiaokesnicdeMBP.mshome.net>
2022-09-02 13:04:25 +0800

    fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,14 +3,14 @@ type tokenData = {
   name: string;
   id:number
 }
-const getToken = (data:tokenData) => {
+export const getToken = (data:tokenData) => {
   request({
     url: "/token",
     method: "post",
     data
   })
 }
-const getUserInfo = (id:number) => {
+export const getUserInfo = (id:number) => {
   request({
     url: "/token",
     method: "get",
